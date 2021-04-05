4173da81b43d2de8a82467b2525851329c523c7d
Abumuslim-1976 <78642826+Abumuslim-1976@users.noreply.github.com>
2021-04-05 16:16:46 +0500

    operator can`t post method

--- src/main/java/uz/pdp/dataresttwo/config/UserSecurity.java
+++ src/main/java/uz/pdp/dataresttwo/config/UserSecurity.java
@@ -33,61 +33,61 @@ public class UserSecurity extends WebSecurityConfigurerAdapter {
 
 //                =======Basket yo`liga murojaat======
                 .antMatchers(HttpMethod.GET, "/api/basket/**").hasAnyRole("SUPER_ADMIN", "MODERATOR", "OPERATOR")
-                .antMatchers(HttpMethod.POST,"/api/basket/**").hasAnyRole("SUPER_ADMIN", "MODERATOR", "OPERATOR")
+                .antMatchers(HttpMethod.POST,"/api/basket/**").hasAnyRole("SUPER_ADMIN", "MODERATOR")
                 .antMatchers(HttpMethod.PUT,"/api/basket/**").hasAnyRole("SUPER_ADMIN", "MODERATOR")
                 .antMatchers("/api/basket/**").hasRole("SUPER_ADMIN")
 
 //                =======Blog yo`liga murojaat======
                 .antMatchers(HttpMethod.GET, "/api/blog/**").hasAnyRole("SUPER_ADMIN", "MODERATOR", "OPERATOR")
-                .antMatchers(HttpMethod.POST,"/api/blog/**").hasAnyRole("SUPER_ADMIN", "MODERATOR", "OPERATOR")
+                .antMatchers(HttpMethod.POST,"/api/blog/**").hasAnyRole("SUPER_ADMIN", "MODERATOR")
                 .antMatchers(HttpMethod.PUT,"/api/blog/**").hasAnyRole("SUPER_ADMIN", "MODERATOR")
                 .antMatchers("/api/blog/**").hasRole("SUPER_ADMIN")
 
 //                =======Category yo`liga murojaat======
                 .antMatchers(HttpMethod.GET, "/api/category/**").hasAnyRole("SUPER_ADMIN", "MODERATOR", "OPERATOR")
-                .antMatchers(HttpMethod.POST,"/api/category/**").hasAnyRole("SUPER_ADMIN", "MODERATOR", "OPERATOR")
+                .antMatchers(HttpMethod.POST,"/api/category/**").hasAnyRole("SUPER_ADMIN", "MODERATOR")
                 .antMatchers(HttpMethod.PUT,"/api/category/**").hasAnyRole("SUPER_ADMIN", "MODERATOR")
                 .antMatchers("/api/category/**").hasRole("SUPER_ADMIN")
 
 //                =======Characteristic yo`liga murojaat======
                 .antMatchers(HttpMethod.GET, "/api/characteristic/**").hasAnyRole("SUPER_ADMIN", "MODERATOR", "OPERATOR")
-                .antMatchers(HttpMethod.POST,"/api/characteristic/**").hasAnyRole("SUPER_ADMIN", "MODERATOR", "OPERATOR")
+                .antMatchers(HttpMethod.POST,"/api/characteristic/**").hasAnyRole("SUPER_ADMIN", "MODERATOR")
                 .antMatchers(HttpMethod.PUT,"/api/characteristic/**").hasAnyRole("SUPER_ADMIN", "MODERATOR")
                 .antMatchers("/api/characteristic/**").hasRole("SUPER_ADMIN")
 
 //                =======Comment yo`liga murojaat======
                 .antMatchers(HttpMethod.GET, "/api/comment/**").hasAnyRole("SUPER_ADMIN", "MODERATOR", "OPERATOR")
-                .antMatchers(HttpMethod.POST,"/api/comment/**").hasAnyRole("SUPER_ADMIN", "MODERATOR", "OPERATOR")
+                .antMatchers(HttpMethod.POST,"/api/comment/**").hasAnyRole("SUPER_ADMIN", "MODERATOR")
                 .antMatchers(HttpMethod.PUT,"/api/comment/**").hasAnyRole("SUPER_ADMIN", "MODERATOR")
                 .antMatchers("/api/comment/**").hasRole("SUPER_ADMIN")
 
 //                =======Order yo`liga murojaat======
                 .antMatchers(HttpMethod.GET, "/api/orders/**").hasAnyRole("SUPER_ADMIN", "MODERATOR", "OPERATOR")
-                .antMatchers(HttpMethod.POST,"/api/orders/**").hasAnyRole("SUPER_ADMIN", "MODERATOR", "OPERATOR")
+                .antMatchers(HttpMethod.POST,"/api/orders/**").hasAnyRole("SUPER_ADMIN", "MODERATOR")
                 .antMatchers(HttpMethod.PUT,"/api/orders/**").hasAnyRole("SUPER_ADMIN", "MODERATOR")
                 .antMatchers("/api/orders/**").hasRole("SUPER_ADMIN")
 
 //                =======Product yo`liga murojaat======
                 .antMatchers(HttpMethod.GET, "/api/product/**").hasAnyRole("SUPER_ADMIN", "MODERATOR", "OPERATOR")
-                .antMatchers(HttpMethod.POST,"/api/product/**").hasAnyRole("SUPER_ADMIN", "MODERATOR", "OPERATOR")
+                .antMatchers(HttpMethod.POST,"/api/product/**").hasAnyRole("SUPER_ADMIN", "MODERATOR")
                 .antMatchers(HttpMethod.PUT,"/api/product/**").hasAnyRole("SUPER_ADMIN", "MODERATOR")
                 .antMatchers("/api/product/**").hasRole("SUPER_ADMIN")
 
 //                =======Property yo`liga murojaat======
                 .antMatchers(HttpMethod.GET, "/api/property/**").hasAnyRole("SUPER_ADMIN", "MODERATOR", "OPERATOR")
-                .antMatchers(HttpMethod.POST,"/api/property/**").hasAnyRole("SUPER_ADMIN", "MODERATOR", "OPERATOR")
+                .antMatchers(HttpMethod.POST,"/api/property/**").hasAnyRole("SUPER_ADMIN", "MODERATOR")
                 .antMatchers(HttpMethod.PUT,"/api/property/**").hasAnyRole("SUPER_ADMIN", "MODERATOR")
                 .antMatchers("/api/property/**").hasRole("SUPER_ADMIN")
 
 //               ======Supplier yo`liga murojaat======
                 .antMatchers(HttpMethod.GET, "/api/supplier/**").hasAnyRole("SUPER_ADMIN", "MODERATOR", "OPERATOR")
-                .antMatchers(HttpMethod.POST,"/api/supplier/**").hasAnyRole("SUPER_ADMIN", "MODERATOR", "OPERATOR")
+                .antMatchers(HttpMethod.POST,"/api/supplier/**").hasAnyRole("SUPER_ADMIN", "MODERATOR")
                 .antMatchers(HttpMethod.PUT,"/api/supplier/**").hasAnyRole("SUPER_ADMIN", "MODERATOR")
                 .antMatchers("/api/supplier/**").hasRole("SUPER_ADMIN")
 
 //              ======User yo`liga murojaat======
                 .antMatchers(HttpMethod.GET, "/api/users/**").hasAnyRole("SUPER_ADMIN", "MODERATOR", "OPERATOR")
-                .antMatchers(HttpMethod.POST,"/api/users/**").hasAnyRole("SUPER_ADMIN", "MODERATOR", "OPERATOR")
+                .antMatchers(HttpMethod.POST,"/api/users/**").hasAnyRole("SUPER_ADMIN", "MODERATOR")
                 .antMatchers(HttpMethod.PUT,"/api/users/**").hasAnyRole("SUPER_ADMIN", "MODERATOR")
                 .antMatchers("/api/users/**").hasRole("SUPER_ADMIN")
 
